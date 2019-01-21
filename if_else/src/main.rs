a57a324c5e101bbb8da70d4e9972b27de73ef349
sjvasconcello <sjvasconcello@gmail.com>
2019-01-21 17:09:27 -0300

    Videos 3 - 6

---
+++ if_else/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let n = 80 ;
+
+    if n == 45 {
+        println!("Rust!");
+    } else if n > 50 {
+        println!("It is greater than 50!")
+    }
+    else {
+        println!("It was not 45")
+    }
+}
---
+++ variables/src/main.rs
@@ -0,0 +1,15 @@
+// Variable - Video 4
+/*
+u64 dont suport negative 
+i64 suport negative numbers
+f32 is float
+*/
+
+fn main() {
+    let x: u64 = 45; //u32
+    let f: f32 = 6.7 //f32
+
+    let b: bool = false;
+
+
+}
